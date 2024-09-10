102643a25f38c8bafe32d69160d75bada4e452ec
p5droluis <49886339+p5droluis@users.noreply.github.com>
2024-09-10 20:08:09 -0300

    Update hello-server.go

--- hello-server.go
+++ hello-server.go
@@ -7,7 +7,5 @@ func main() {
 http.HandleFunc("/", HelloServer)
 http.ListenAndServe(":13000", nil)
 }
-func HelloServer(w http.ResponseWriter, r
-*http.Request) {
-fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func HelloServer(w http.ResponseWriter, r*http.Request) {fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
